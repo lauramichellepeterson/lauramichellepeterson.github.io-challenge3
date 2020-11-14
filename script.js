71a1e75ac422d988d5cb9476bb262a720e60e02f
Laura Micheller Peterson <michellepeterson.mishmish@gmail.com>
2020-11-14 13:27:38 -0700

    add input variables and getCharacterTotal

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 // Assignment code here
+var characterTotal = 0;
+var useLower = false;
+var useUpper = false;
+var useNumber = false;
+var useSpecial = false;
 
+
+var getCharacterTotal = function() {
+  if (characterTotal < 8 || characterTotal > 128 || characterTotal === "" || (characterTotal/characterTotal) != 1  ) {
+    characterTotal = window.prompt("Enter length of password (8-128).");
+    getCharacterTotal();
+  }
+}
+
+var getUserInputs = function() {
+  getCharacterTotal();
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  getUserInputs();
+
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
